e7780dd60ba94c6e86455d98eaa0d6795ad50e20
Bramhendra Kumar <37215456+bramhendra123@users.noreply.github.com>
2018-09-21 16:22:05 +0530

    Create solution.java
    30 days code of in java of hackerrank ......

---
+++ solution.java
@@ -0,0 +1,24 @@
+//# 30-Days-of-Code
+//7-Arrays
+////////////////////////////////////////////////////////
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            int idx = n - i - 1;
+            System.out.print(arr[idx] + " ");
+        }
+
+        in.close();
+    }
+}
